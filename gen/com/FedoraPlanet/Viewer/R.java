ba2b6c8bc01e13f68bf95873ebb62565c55fa475
Harsha <Harsha@MacBook.local>
2012-12-27 13:05:17 +0530

    new Applogo: credit to GLaDoS @GCI12, renamed captions, v1.1

--- gen/com/FedoraPlanet/Viewer/R.java
+++ gen/com/FedoraPlanet/Viewer/R.java
@@ -13,8 +13,9 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int icon=0x7f020001;
-        public static final int splash=0x7f020002;
-        public static final int splashimage=0x7f020003;
+        public static final int icon2=0x7f020002;
+        public static final int splash=0x7f020003;
+        public static final int splashimage=0x7f020004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
